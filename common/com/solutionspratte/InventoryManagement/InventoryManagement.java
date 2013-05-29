5bd53c3cddaa3bd6d0ab0637f541e13f2ffd3900
hancin <hancin@hancinworld.com>
2013-05-29 18:52:03 -0400

    Start mod coding!

---
+++ common/com/solutionspratte/InventoryManagement/InventoryManagement.java
@@ -0,0 +1,38 @@
+package com.solutionspratte.InventoryManagement;
+
+import com.solutionspratte.InventoryManagement.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * 
+ * @author David Larochelle-Pratte
+ * @license GNU Public License v3 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class InventoryManagement {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+}
---
+++ common/com/solutionspratte/InventoryManagement/lib/Reference.java
@@ -0,0 +1,12 @@
+package com.solutionspratte.InventoryManagement.lib;
+
+/**
+ * 
+ * @author David Larochelle-Pratte
+ * @license GNU Public License v3 (http://www.gnu.org/licenses/gpl.html)
+ */
+public class Reference {
+    public static final String MOD_ID = "CSIM";
+    public static final String MOD_NAME = "Players Inventory Management";
+    public static final String VERSION = "0.0.1";
+}
