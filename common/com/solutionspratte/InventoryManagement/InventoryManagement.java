1da2431e8a3bc2a1f3c74b5a8eb182cac8c07c73
hancin <hancin@hancinworld.com>
2013-05-29 20:56:43 -0400

    Give the baby a name!

--- common/com/solutionspratte/InventoryManagement/InventoryManagement.java
+++ common/com/solutionspratte/InventoryManagement/InventoryManagement.java
@@ -1,6 +1,7 @@
 package com.solutionspratte.InventoryManagement;
 
 import com.solutionspratte.InventoryManagement.item.ModItems;
+import com.solutionspratte.InventoryManagement.lib.Localizations;
 import com.solutionspratte.InventoryManagement.lib.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
@@ -9,6 +10,7 @@ import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 /**
  * 
@@ -22,6 +24,9 @@ public class InventoryManagement {
     @PreInit
     public void preInit(FMLPreInitializationEvent event)
     {
+        String file = Localizations.localeFiles[0];
+        LanguageRegistry.instance().loadLocalization(file, "en_US", true);
+        
         ModItems.init();
     }
     
---
+++ common/com/solutionspratte/InventoryManagement/lib/Localizations.java
@@ -0,0 +1,18 @@
+package com.solutionspratte.InventoryManagement.lib;
+
+/**
+ * Equivalent-Exchange-3
+ * 
+ * Localizations
+ * 
+ * @author pahimar
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+public class Localizations {
+
+    private static final String LANG_RESOURCE_LOCATION = "/mods/csim/lang/";
+
+    public static String[] localeFiles = { LANG_RESOURCE_LOCATION + "en_US.xml"};
+
+}
